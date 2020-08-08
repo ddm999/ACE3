54c34c162a24e01f82172e187ff082d3ea91666b
Daniel Lee <dan.ddm999@gmail.com>
2020-08-08 16:41:51 +0100

    custom medical replacement items
    Reverts to Vanilla FAK / Medikit weights.
    Default bandage is packing, medic also has elastics.
    
    FAK comes with a splint.
    Medikit comes with splints, PAK & surgical kit.
    
    Other than that, mostly additional items to match the original weights.

--- addons/medical_treatment/CfgReplacementItems.hpp
+++ addons/medical_treatment/CfgReplacementItems.hpp
@@ -3,19 +3,21 @@
 // Class names need no special prefix
 class EGVAR(medical,replacementItems) {
     DOUBLES(ItemType,TYPE_FIRST_AID_KIT)[] = {
-        {"ACE_fieldDressing", 1},
-        {"ACE_packingBandage", 1},
-        {"ACE_morphine", 1},
-        {"ACE_tourniquet", 1}
+        {"ACE_packingBandage", 3},
+        {"ACE_morphine", 2},
+        {"ACE_tourniquet", 1},
+        {"ACE_splint", 1}
     };
     DOUBLES(ItemType,TYPE_MEDIKIT)[] = {
-        {"ACE_fieldDressing", 1},
-        {"ACE_packingBandage", 2},
-        {"ACE_epinephrine", 1},
-        {"ACE_morphine", 1},
-        {"ACE_salineIV_250", 1},
-        {"ACE_tourniquet", 1},
-        {"ACE_splint", 2}
+        {"ACE_packingBandage", 10},
+        {"ACE_elasticBandage", 10},
+        {"ACE_morphine", 5},
+        {"ACE_tourniquet", 5},
+        {"ACE_splint", 5},
+        {"ACE_epinephrine", 5},
+        {"ACE_salineIV_250", 4},
+        {"ACE_personalAidKit", 1},
+        {"ACE_surgicalKit", 1}
     };
     ACE_atropine[] = {
         {"ACE_adenosine", 1}
